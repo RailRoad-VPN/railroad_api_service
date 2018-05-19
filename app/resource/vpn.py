b2a701f7c1c2567de993f209df70f32f393c7e24
Artur Karapetov <858683@gmail.com>
2018-05-19 11:16:39 +0300

    refactoring

--- app/resource/vpn.py
+++ app/resource/vpn.py
@@ -1,12 +1,10 @@
 import json
 import sys
-import uuid as uuidlib
 from http import HTTPStatus
 
 from flask import make_response, Response, request
 
 from app.exception import RailRoadAPIError
-from app.service import UserService
 
 sys.path.insert(0, '../rest_api_library')
 from api import ResourceAPI
@@ -35,6 +33,7 @@ class VPNServersAPI(ResourceAPI):
 
     def get(self, uuid: str = None) -> Response:
         if 'version' in request.args:
+            # return version and state version
             data = {
                 'version': 42,
                 'state_version': 24
@@ -44,78 +43,83 @@ class VPNServersAPI(ResourceAPI):
             resp = make_response(json.dumps(response_data.serialize()), HTTPStatus.OK)
             return resp
         if 'state' in request.args:
-            data = [{
-                "id": "16fd2706-8baf-433b-82eb-8c7fada847da",
-                "state_version": "2",
-                "status": "2",
-                "bandwidth": "123456",
-                "load": "30",
-            }, {
-                "id": "8d0359a4-64d1-4906-b113-2e36259dd128",
-                "state_version": "2",
-                "status": "3",
-                "bandwidth": "123456788",
-                "load": "10",
-            }]
+            # servers state info only
+            data = [
+                {
+                    "id": "16fd2706-8baf-433b-82eb-8c7fada847da",
+                    "state_version": "2",
+                    "status": "2",
+                    "bandwidth": "123456",
+                    "load": "30",
+                },
+                {
+                    "id": "8d0359a4-64d1-4906-b113-2e36259dd128",
+                    "state_version": "2",
+                    "status": "3",
+                    "bandwidth": "123456788",
+                    "load": "10",
+                }]
             response_data = APIResponse(status=APIResponseStatus.success.value, code=HTTPStatus.OK,
                                         data=data)
             resp = make_response(json.dumps(response_data.serialize()), HTTPStatus.OK)
             return resp
         if uuid is None:
             # list of all servers
-            data = [{
-                "id": "16fd2706-8baf-433b-82eb-8c7fada847da",
-                "version": "1",
-                "state_version": "2",
-                "type": "1",
-                "status": "2",
-                "bandwidth": "123456223",
-                "load": "30",
-                "geo": {
-                    "latitude": "55.7558",
-                    "longitude": "37.6173",
-                    "decode": {
-                        "country": 0,
-                        "state": 0,
-                        "city": 0,
-                        "region": {
-                            "common": 0,
-                            "dvd": 0,
-                            "xbox360": 0,
-                            "xboxone": 0,
-                            "playstation3": 0,
-                            "playstation4": 0,
-                            "nintendo": 0
+            data = [
+                {
+                    "id": "16fd2706-8baf-433b-82eb-8c7fada847da",
+                    "version": "1",
+                    "state_version": "2",
+                    "type": "1",
+                    "status": "2",
+                    "bandwidth": "123456223",
+                    "load": "30",
+                    "geo": {
+                        "latitude": "55.7558",
+                        "longitude": "37.6173",
+                        "decode": {
+                            "country": 0,
+                            "state": 0,
+                            "city": 0,
+                            "region": {
+                                "common": 0,
+                                "dvd": 0,
+                                "xbox360": 0,
+                                "xboxone": 0,
+                                "playstation3": 0,
+                                "playstation4": 0,
+                                "nintendo": 0
+                            }
                         }
                     }
-                }
-            }, {
-                "id": "8d0359a4-64d1-4906-b113-2e36259dd128",
-                "version": "2",
-                "state_version": "2",
-                "type": "2",
-                "status": "3",
-                "bandwidth": "123456431",
-                "load": "0",
-                "geo": {
-                    "latitude": "55.7558",
-                    "longitude": "37.6173",
-                    "decode": {
-                        "country": 0,
-                        "state": 0,
-                        "city": 0,
-                        "region": {
-                            "common": 0,
-                            "dvd": 0,
-                            "xbox360": 0,
-                            "xboxone": 0,
-                            "playstation3": 0,
-                            "playstation4": 0,
-                            "nintendo": 0
+                },
+                {
+                    "id": "8d0359a4-64d1-4906-b113-2e36259dd128",
+                    "version": "2",
+                    "state_version": "2",
+                    "type": "2",
+                    "status": "3",
+                    "bandwidth": "123456431",
+                    "load": "0",
+                    "geo": {
+                        "latitude": "55.7558",
+                        "longitude": "37.6173",
+                        "decode": {
+                            "country": 0,
+                            "state": 0,
+                            "city": 0,
+                            "region": {
+                                "common": 0,
+                                "dvd": 0,
+                                "xbox360": 0,
+                                "xboxone": 0,
+                                "playstation3": 0,
+                                "playstation4": 0,
+                                "nintendo": 0
+                            }
                         }
                     }
-                }
-            }]
+                }]
             response_data = APIResponse(status=APIResponseStatus.success.value, code=HTTPStatus.OK,
                                         data=data)
             resp = make_response(json.dumps(response_data.serialize()), HTTPStatus.OK)
